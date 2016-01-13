789c79a2b96dc8a9d25049a6c96c7758d00e88d3
Konrad Grüner <konraifen88@googlemail.com>
2016-01-13 21:04:37 +0100

    extending new mine

--- public/javascripts/mainPage.js
+++ public/javascripts/mainPage.js
@@ -6,19 +6,8 @@ var mainPage = angular.module('mainPage', []);
 mainPage.controller('mainCtrl', function ($scope) {
 
     $scope.getLobbies = function () {
-        $scope.lobbiesObj = JSON.parse($("#lobbyData").text());
-        $scope.lobbies = [];
-        for (var i = 0; i < Object.keys($scope.lobbiesObj).length; i++) {
-            var key = Object.keys($scope.lobbiesObj)[i];
-            var val = $scope.lobbiesObj[key];
-            $scope.lobbies.push({
-                lobbyName: key,
-                users: val
-            });
-        }
-        console.log($scope.lobbies);
+        $scope.lobbies = JSON.parse($("#lobbyData").text());
     };
-    $scope.lobbies = [];
     $scope.getLobbies();
 });
 
