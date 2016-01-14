796c2283ff6a6ffd301360e0541f70ff9ff331ef
Konraifen88 <konraifen88@googlemail.com>
2016-01-14 17:43:51 +0100

    adding back button to navbar

---
+++ public/javascripts/navBar.js
@@ -0,0 +1,12 @@
+/**
+* If everything works right this class was
+* created by Konraifen88 on 14.01.2016.
+* If it doesn't work I don't know who the hell wrote it.
+*/
+
+$(function() {
+    if ((window.location.pathname === "/")) {
+        $(backButton).hide()
+    }
+});
+
